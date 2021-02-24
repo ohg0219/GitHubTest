a8f5ec9864f9e91395b3645a7f71c18c45aa94fe
Cho Yong-il <ceradyd@naver.com>
2021-02-25 00:07:32 +0900

    전체글 / 글 하나 보기 틀만 만들어 놓음

--- src/main/java/com/spring/web/board/impl/BoardDAO.java
+++ src/main/java/com/spring/web/board/impl/BoardDAO.java
@@ -3,6 +3,8 @@ package com.spring.web.board.impl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.spring.web.board.BoardVO;
 import com.spring.web.board.connection.ConnectionUtil;
@@ -48,5 +50,32 @@ public class BoardDAO {
 			ConnectionUtil.close(pstmt, conn);
 		}
 	}
-
-}
+	
+	public BoardVO getBoard(BoardVO vo) {
+		try {
+			conn = ConnectionUtil.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			ConnectionUtil.close(rs, pstmt, conn);
+		}
+		return null;
+	}
+	
+	public List<BoardVO> getBoardList (BoardVO vo) {
+		List<BoardVO> boardList = new ArrayList<BoardVO>();
+		try {
+			conn = ConnectionUtil.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			ConnectionUtil.close(rs, pstmt, conn);
+		}
+		return boardList;
+	}
+	
+}//end class
