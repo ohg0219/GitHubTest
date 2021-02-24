badb1b6ab6cf48423174f2f9b5c10e25cccbf965
Cho Yong-il <ceradyd@naver.com>
2021-02-24 09:11:58 +0900

    7744

--- src/main/java/com/spring/web/board/impl/BoardDAO.java
+++ src/main/java/com/spring/web/board/impl/BoardDAO.java
@@ -1,9 +1,11 @@
 package com.spring.web.board.impl;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 
 public class BoardDAO {
 public Connection conn = null;
+public ResultSet rs = null;
 public int i = 0;
 public int j = 0;
 }
