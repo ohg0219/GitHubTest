8c66c56c74bc54f369dfd40fb4e808b5178f65a1
DESKTOP-GVLB0H4\82109 <jinsu@naver.com>
2021-02-24 13:52:57 +0900

    service

---
+++ src/main/java/com/spring/web/board/impl/Service.java
@@ -0,0 +1,15 @@
+package com.spring.web.board.impl;
+
+import java.util.List;
+
+import com.spring.web.board.BoardVO;
+
+public interface Service {
+	BoardVO getBoard();
+	
+	List<BoardVO> getBoardList();
+	
+	void delete();
+	
+	void update();
+}
