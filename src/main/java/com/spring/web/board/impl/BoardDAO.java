63d713ecb04f68b75542aaadda573435bea8fc9c
Cho Yong-il <ceradyd@naver.com>
2021-02-24 13:37:35 +0900

    preparedstatement 업데이트

--- src/main/java/com/spring/web/board/impl/BoardDAO.java
+++ src/main/java/com/spring/web/board/impl/BoardDAO.java
@@ -1,10 +1,12 @@
 package com.spring.web.board.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class BoardDAO {
 public Connection conn = null;
+public PreparedStatement pstmt = null;
 public ResultSet rs = null;
 public int i = 0;
 public int j = 0;
