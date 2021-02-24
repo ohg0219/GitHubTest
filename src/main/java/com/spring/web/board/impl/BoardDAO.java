3ec415f5830f173287452432f3fcfa0828d45771
Cho Yong-il <ceradyd@naver.com>
2021-02-24 23:59:10 +0900

    DAO 기본 세팅(새글등록/수정/삭제) 틀만 만들었음

--- src/main/java/com/spring/web/board/impl/BoardDAO.java
+++ src/main/java/com/spring/web/board/impl/BoardDAO.java
@@ -4,9 +4,49 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import com.spring.web.board.BoardVO;
+import com.spring.web.board.connection.ConnectionUtil;
+
 public class BoardDAO {
-public Connection conn = null;
-public PreparedStatement pstmt = null;
-public ResultSet rs = null;
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	String sql = "";
+
+	public void insertBoard(BoardVO vo) {//새글 등록
+		try {
+			conn = ConnectionUtil.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionUtil.close(pstmt, conn);
+		}
+	}
+
+	public void updateBoard(BoardVO vo) {//수정
+		try {
+			conn = ConnectionUtil.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			ConnectionUtil.close(pstmt, conn);
+		}
+	}
+
+	public void deleteBoard(BoardVO vo) {//삭제
+		try {
+			conn = ConnectionUtil.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionUtil.close(pstmt, conn);
+		}
+	}
 
 }
