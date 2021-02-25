ec3cf47f50efc6d90844484130af9049f2ab20a6
DESKTOP-GVLB0H4\82109 <jinsu@naver.com>
2021-02-25 14:19:05 +0900

    임시저장

---
+++ src/main/java/com/spring/web/board/impl/BoardServiceImpl.java
@@ -0,0 +1,29 @@
+package com.spring.web.board.impl;
+
+import java.util.List;
+
+import com.spring.web.board.BoardVO;
+
+public class BoardServiceImpl implements BoardService {
+
+	@Override
+	public BoardVO getBoard(int seq) {
+		return null;
+	}
+
+	@Override
+	public List<BoardVO> getBoardList() {
+		return null;
+	}
+
+	@Override
+	public void delete() {
+		
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+}
